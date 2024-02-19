691b6431e386f883699df2a94e761d87816ccd8c
raky <raky@daum.net>
2024-02-19 12:54:29 +0900

    workflow build add

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,14 @@
 FROM python:3.12-slim
 
+# bash 설정
+RUN echo "alias egrep='egrep --color=auto'" >> ~/.bashrc
+RUN echo "alias fgrep='fgrep --color=auto'" >> ~/.bashrc
+RUN echo "alias grep='grep --color=auto'" >> ~/.bashrc
+RUN echo "alias l='ls -CF'" >> ~/.bashrc
+RUN echo "alias la='ls -A'" >> ~/.bashrc
+RUN echo "alias ll='ls -alF'" >> ~/.bashrc
+RUN echo "alias ls='ls --color=auto'" >> ~/.bashrc
+
 WORKDIR /app
 
 COPY requirements.txt .
--- main.py
+++ main.py
@@ -1,15 +1,54 @@
-from fastapi import FastAPI
+from typing import List
+
+from fastapi import FastAPI, Query, Path, Body
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
-@app.get("/api/")
-def read_root():
-    return {"Hello": "World 2"}
 
-@app.get("/api/info")
-def read_root():
-    return {"url": "/api/info"}
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
 
-@app.get("/api/info/{item_id}")
-def read_root(item_id):
-    return {"url": "/api/info", "item_id": item_id}
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(*, item_id: int = Path(..., title="The ID of the item to get", ge=1, le=1000), q: List[str] | None = Query(None, max_length=5, title="타이틀", description="검색값", deprecated=True)):
+    return {"item_id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item = Body(openapi_examples={
+                "normal": {
+                    "summary": "A normal example",
+                    "description": "A **normal** item works correctly.",
+                    "value": {
+                        "name": "Foo",
+                        "description": "A very nice Item",
+                        "price": 35.4,
+                        "tax": 3.2,
+                    },
+                },
+                "converted": {
+                    "summary": "An example with converted data",
+                    "description": "FastAPI can convert price `strings` to actual `numbers` automatically",
+                    "value": {
+                        "name": "Bar",
+                        "price": "35.4",
+                    },
+                },
+                "invalid": {
+                    "summary": "Invalid data is rejected with an error",
+                    "value": {
+                        "name": "Baz",
+                        "price": "thirty five point four",
+                    },
+                },
+            },)):
+    return {"item_name": item.name, "item_id": item_id}
